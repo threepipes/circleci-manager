4f239c134eaee78d633735cf3d80434c5e10de5c
threepipes <threepipes@gmail.com>
2022-11-27 18:47:29 +0900

    add a test for UpdateOrCreateVariable()

--- client_test.go
+++ client_test.go
@@ -2,16 +2,17 @@ package cli
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	mock_cli "github.com/threepipes/circleci-env/mock/cli"
-
 	"github.com/grezar/go-circleci"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
+	mock_cli "github.com/threepipes/circleci-env/mock/cli"
 )
 
 func Test_getFoundAndNotFoundVariables(t *testing.T) {
@@ -167,5 +168,105 @@ func TestClient_DeleteVariablesInteractive(t *testing.T) {
 	assert.Equal(t, 1, info["GET "+expectedListURL], "Expected number of list API call is wrong")
 	for _, d := range expectedDeleteURLs {
 		assert.Equal(t, 1, info["DELETE "+d], "Expected number of delete API call is wrong")
+	}
+}
+
+func updateOrCreateScaffold(t *testing.T, expected *circleci.ProjectVariable, exists bool) func() {
+	expectedGetURL := apiBaseURL + "/envvar/" + expected.Name
+	expectedCreateURL := apiBaseURL + "/envvar"
+	if exists {
+		resp, err := httpmock.NewJsonResponder(200, expected)
+		if err != nil {
+			t.Errorf("Failed to convert the expected variable: %v", err)
+		}
+		httpmock.RegisterResponder("GET", expectedGetURL, resp)
+	} else {
+		httpmock.RegisterResponder("GET", expectedGetURL, httpmock.NewNotFoundResponder(nil))
+	}
+	httpmock.RegisterResponder("POST", expectedCreateURL,
+		func(r *http.Request) (*http.Response, error) {
+			var pv circleci.ProjectVariable
+			err := json.NewDecoder(r.Body).Decode(&pv)
+			if err != nil {
+				msg := fmt.Sprintf("Failed to convert the expected variable: %v", err)
+				return httpmock.NewStringResponse(500, msg), nil
+			}
+			resp, err := httpmock.NewJsonResponse(201, expected)
+			if err != nil {
+				msg := fmt.Sprintf("Failed to convert the expected variable: %v", err)
+				return httpmock.NewStringResponse(500, msg), nil
+			}
+			return resp, nil
+		})
+	checker := func() {
+		info := httpmock.GetCallCountInfo()
+		assert.Equal(t, 1, info["GET "+expectedGetURL], "Expected number of get API call is wrong")
+		assert.Equal(t, 1, info["POST "+expectedCreateURL], "Expected number of post API call is wrong")
+	}
+	return checker
+}
+
+func TestClient_UpdateOrCreateVariable(t *testing.T) {
+	type args struct {
+		pv            *circleci.ProjectVariable
+		alreadyExists bool
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "The variable doesn't exist",
+			args: args{
+				pv: &circleci.ProjectVariable{
+					Name:  "test1",
+					Value: "xxxxEnv1",
+				},
+				alreadyExists: false,
+			},
+		},
+		{
+			name: "The variable already exists",
+			args: args{
+				pv: &circleci.ProjectVariable{
+					Name:  "test2",
+					Value: "xxxxEnv2",
+				},
+				alreadyExists: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			checker := updateOrCreateScaffold(t, tt.args.pv, tt.args.alreadyExists)
+			defer checker()
+
+			config := circleci.DefaultConfig()
+			config.HTTPClient = http.DefaultClient
+			config.Token = testAPIToken
+			ci, err := circleci.NewClient(config)
+			if err != nil {
+				t.Error(err)
+			}
+
+			ctrl := gomock.NewController(t)
+			ui := mock_cli.NewMockUI(ctrl)
+			if tt.args.alreadyExists {
+				ui.EXPECT().YesNo(gomock.Any()).Return(true, nil)
+			}
+
+			c := &Client{
+				ci:          ci,
+				projectSlug: projectSlug,
+				ui:          ui,
+				token:       testAPIToken,
+			}
+			if err := c.UpdateOrCreateVariable(context.Background(), tt.args.pv.Name, tt.args.pv.Value); err != nil {
+				t.Errorf("Client.UpdateOrCreateVariable() error = %v", err)
+			}
+		})
 	}
 }
